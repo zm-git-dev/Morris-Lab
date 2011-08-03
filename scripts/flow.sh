464e5456d58dcc094d72bd925f29be1d3c76c4f7
Chris Warth <csw@well.com>
2011-08-03 13:43:01 -0700

    Calculate statistics as number of available reads at each point.   This is more reliable than tracking the reads that are removed at each step. Reorganized the code to collect statistics at the end instead of mixing the collection in with the other code.
     Please enter the commit message for your changes. Lines starting

--- scripts/flow.sh
+++ scripts/flow.sh
@@ -4,6 +4,46 @@
 # Usage:  flow1.sh <reads.fa>  
 #
 
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script run the test1 or test2 over a machine.
+
+OPTIONS:
+   -h      Show this message
+   -t      Test type, can be ‘test1′ or ‘test2′
+   -r      Server address
+   -p      Server root password
+   -v      Verbose
+EOF
+}
+
+VERBOSE=
+STATONLY=
+while getopts "hsv" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         s)
+             STATSONLY=1
+	     echo "stats only"
+             ;;
+         v)
+             VERBOSE=1
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+
 logfile=/tmp/mylog
 
 echo >$logfile
@@ -25,7 +65,8 @@ function readc() { echo $(( $(cat $* | wc -l) / 4 )); }
 
 exec 2>errors.txt
 
-declare -A counts
+declare -A reads
+declare -A alignments
 
 ADAPTER="ATCTCGTATGCCGTCTTCTGCTTG"
 MORRIS="/home/csw/Morris-Lab"
@@ -45,64 +86,89 @@ if [ -t "1" ]; then
 fi
 
 
-log_msg "Trimming adapter and filtering bad reads..."
+if [[ "$STATSONLY" -ne 1 ]]
+then
+    log_msg "Trimming adapter and filtering bad reads..."
 
-fastx_clipper -a ${ADAPTER} -l 20 -Q33 -v < ${rawreads} > ${basename}.multilen.fastq 2>clipstats.txt
+    fastx_clipper -a ${ADAPTER} -l 20 -Q33 -v < ${rawreads} > ${basename}.multilen.fastq 2>clipstats.txt
+    echo "fastx_clipper returned $?" >&2
+
+    log_msg "aligning to rRNA bowtie..."
+    bowtie --al contaminated.fq --un uncontaminated.fq /home/csw/Morris-Lab/ref-seq/contaminants/contaminants ${basename}.multilen.fastq -S  >${basename}.rrna.sam  
+    echo "bowtie returned $?" >&2
+
+    goodreads=$(readc uncontaminated.fq)
+    log_msg "aligning ${goodreads} reads to  MM9 genome with tophat..."
+    tophat --segment-length 10 --segment-mismatches 0 -G ~/Morris-Lab/ref-seq/mm9/refseq_knowngenes.gtf  /mm9/mm9 uncontaminated.fq
+    echo "tophat returned $?" >&2
+
+    log_msg "filtering out imperfect alignments..."
+    samtools view -h tophat_out/accepted_hits.bam | awk '/^@/ || /NM:i:0\>/'| samtools view -Sb -o tophat_out/accepted_hits_perfect.bam /dev/stdin 
+    
+    log_msg "detecting exon converage..."
+    # intersect those alignmets with exons of known genes
+    intersectBed -abam tophat_out/accepted_hits_perfect.bam -b ${REFSEQ}/mm9/refseq_knowngenes_exons.bed -s -f 1.0 -wa -wb -bed > tophat_out/overlaps_exons.bed
+    echo "intersectBed returned $?" >&2
+
+    # Eliminate reads that map to more than a single gene
+    awk '{ print $_"\t"$4; }' <tophat_out/overlaps_exons.bed | sort -k 13 | uniq -f 12 -u | cut -f 1-12 >tophat_out/overlaps_exons_uniq.bed
+fi
+
 
 # collect statistics
 stats=($(awk '$2 ~ /^[0-9]+$/ {print $2 }' <clipstats.txt))
-counts[raw]=${stats[0]}
-counts[tooshort]=${stats[2]}
-counts[adapter]=${stats[3]}
-counts[uncalled]=${stats[4]}
+reads[raw]=${stats[0]}
+reads[tooshort]=$(( ${reads[raw]} - ${stats[2]} ))
+reads[adapter]=$(( ${reads[tooshort]} - ${stats[3]} ))
+reads[uncalled]=$(( ${reads[adapter]} - ${stats[4]} ))
 
-log_msg "aligning to rRNA bowtie..."
-bowtie --al contaminated.fq --un uncontaminated.fq /home/csw/Morris-Lab/ref-seq/contaminants/contaminants ${basename}.multilen.fastq -S  >${basename}.rrna.sam  
 
-counts[rrna]=$(readc contaminated.fq)
+log_msg "Counting rRNA alignments..."
+reads[rrna]=$(readc uncontaminated.fq)
 
-# assert contaminated + uncontaminated == raw
-reads=$(readc uncontaminated.fq)
-log_msg "aligning ${reads} reads to  MM9 genome with tophat..."
-tophat --segment-length 14 --segment-mismatches 0 -G ~/Morris-Lab/ref-seq/mm9/refseq_knowngenes.gtf  /mm9/mm9 uncontaminated.fq
+# Count how many failed to align
+log_msg "Counting failed alignments..."
+reads[unaligned]=$(( $(samtools view tophat_out/accepted_hits.bam | awk '{print $1}' |sort|uniq|wc -l) ))
+alignments[unaligned]=$(samtools view tophat_out/accepted_hits.bam | wc -l )
 
-# get how many failed to align
-counts[unaligned]=$(( $(readc uncontaminated.fq) - $(samtools view tophat_out/accepted_hits.bam | awk '/\<NH:i:[0-9]+\>/ {print $1}' |sort|uniq|wc -l) ))
 
-# how many reads mapped to multiple loci on the genome...
-counts[nonuniq]=$(( $(samtools view tophat_out/accepted_hits.bam | awk '!/\<NH:i:1\>/ {print $1}' |sort|uniq|wc -l) ))
+# Count how many imperfect alignments
+log_msg "Counting imperfect alignments..."
+reads[imperfect]=$(($(samtools view tophat_out/accepted_hits_perfect.bam | awk '{print $1}' | sort|uniq|wc -l) ))
 
-log_msg "filtering unique alignments..."
-# Assemble only those alignments that were unique on the genome.
-samtools view -h tophat_out/accepted_hits.bam | awk '/^@/ || /NH:i:1\>/'| samtools view -Sb -o tophat_out/accepted_hits_uniq.bam /dev/stdin 
-
-log_msg "detecting exon converage..."
-# intersect those alignmets with exons of known genes
-intersectBed -abam tophat_out/accepted_hits_uniq.bam -b ${REFSEQ}/mm9/refseq_knowngenes_exons.bed -s -f 1.0 -wa -wb -bed > tophat_out/overlaps_exons.bed
+alignments[imperfect]=$(samtools view tophat_out/accepted_hits_perfect.bam | wc -l)
 
 # how many aligned reads fell outside of known exons
-counts[introns]=$(( $(samtools view tophat_out/accepted_hits_uniq.bam | wc -l) - $(awk '{print $4}' <tophat_out/overlaps_exons.bed | sort|uniq|wc -l) ))
+log_msg "Counting reads outside of exons..."
+reads[introns]=$(awk '{print $4}' <tophat_out/overlaps_exons.bed | sort|uniq|wc -l)
+alignments[introns]=$(wc -l <tophat_out/overlaps_exons.bed)
 
 # count reads that overlap multiple features at a single locus
-counts[mult_gene]=$(awk '{ print $4; }' <tophat_out/overlaps_exons.bed | sort | uniq -c | awk '$1 !~ /1/' | wc -l )
+log_msg "Counting overlaps with multiple features..."
+reads[mult_gene]=$(awk '{ print $4; }' <tophat_out/overlaps_exons_uniq.bed | sort | uniq -c | wc -l )
 
-# Eliminate reads that map to more than a single gene
-awk '{ print $_"\t"$4; }' <tophat_out/overlaps_exons.bed | sort -k 13 | uniq -f 12 -u | cut -f 1-12 >tophat_out/overlaps_exons_uniq.bed
-
+alignments[mult_gene]=$(wc -l <tophat_out/overlaps_exons_uniq.bed)
+    
 log_msg "preparing coverage statistics..."
 # prepare experimental statistics
-python ${SCRIPTS}/rpos_dist.py ${REFSEQ}/mm9/refseq_knowngenes.txt tophat_out/overlaps_exons_uniq.bed >tophat_out/aligned_position_stats.txt
+python ${SCRIPTS}/rpos_dist.py ${REFSEQ}/mm9/refseq_knowngenes.txt tophat_out/overlaps_exons_uniq.bed  >tophat_out/aligned_position_stats.txt 2>rpos_dist_stats.txt
+echo "python rpos_dist.py returned $?" >&2
 
 log_msg "graphing results..."
 ${SCRIPTS}/plot_rpos_vs_region.sh <tophat_out/aligned_position_stats.txt
+log_msg " "
+sleep 3
+echo 
 
-printf "%20s\t%d\n" "Raw reads" ${counts[raw]}
-printf "%20s\t%d\n" "uncalled base 'N'" ${counts[uncalled]}
-printf "%20s\t%d\n" "adapter only" ${counts[adapter]}
-printf "%20s\t%d\n" "too short (<20nt)" ${counts[tooshort]}
-printf "%20s\t%d\n" "rRNA-spike" ${counts[rrna]}
-printf "%20s\t%d\n" "unaligned" ${counts[unaligned]}
-printf "%20s\t%d\n" "multiply aligned" ${counts[nonuniq]}
-printf "%20s\t%d\n" "outside exons" ${counts[introns]}
-printf "%20s\t%d\n" "multiple genes" ${counts[mult_gene]}
+printf "%20s\t%d\t%d\n" "Raw reads" ${reads[raw]} ${alignments[raw]}
+printf "%20s\t%d\t%d\n" "too short (<20nt)" ${reads[tooshort]} ${alignments[tooshort]}
+printf "%20s\t%d\t%d\n" "adapter only" ${reads[adapter]} ${alignments[adapter]}
+printf "%20s\t%d\t%d\n" "uncalled base 'N'" ${reads[uncalled]} ${alignments[uncalled]}
+printf "%20s\t%d\t%d\n" "rRNA-spike" ${reads[rrna]} ${alignments[rrna]}
+printf "%20s\t%d\t%d\n" "unaligned" ${reads[unaligned]} ${alignments[unaligned]}
+printf "%20s\t%d\t%d\n" "imperfect alignments" ${reads[imperfect]} ${alignments[imperfect]}
+printf "%20s\t%d\t%d\n" "outside exons" ${reads[introns]} ${alignments[introns]}
+printf "%20s\t%d\t%d\n" "multiple genes" ${reads[mult_gene]} ${alignments[mult_gene]}
 
+echo
+cat rpos_dist_stats.txt
