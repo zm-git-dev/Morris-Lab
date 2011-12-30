7cfde3093c0122c6b6b5d35d9dcdb2edf2a26a2b
Chris Warth <cswarth@gmail.com>
2011-12-29 18:59:17 -0800

    scripts for calculating fpkm and making a scatter plot of fpkm values.

---
+++ scripts/calculate_fpkm.py
@@ -0,0 +1,142 @@
+# calculate_fpkm.py
+#
+# Calculate FPKM values for each gene to which a set of reads has been mapped.
+# This script reads from its standard input and sends results to its standard output.
+#
+# usage: python ${SCRIPTS}/calculate_fpkm.py < tophat_out/aligned_position_stats.txt
+# options:
+#	-n <depth>	minimum average depth.  Ignore any genes that do not have at
+#			least <depth> mapped reads.   Note that mindepth
+#			is different from minimum depth;  Depth is measured at
+#			a single locus.   depth is measured across the entire
+#			gene.
+#
+# The input file for this script typically comes from rpos_dist.py
+#
+# input format looks like this:
+#    read name       refseq              gene     genelen    region    %    gene%
+# ILLUMINA-D43DB5  NM_001164655    9530053A07Rik  3083        CDS    39.80  39.16
+#
+# This script is only interested in three of these columns:
+# 	read_name -- unique name assigned to thie read by the illumina sequencer
+# 	refseq	-- refseq name for the gene that this read maps to.
+# 	genelen -- length of this gene, in bases (including 5'UTR, CDS, and 3'UTR)
+#
+# FPKM stands for Fragments Per Kilobase of mapped exon per Million reads.
+#
+
+
+import csv
+import sys
+import getopt
+import math
+import errno
+import exceptions
+import collections
+
+
+import util
+
+global _debug               
+_debug = False                  
+
+
+class Gene:
+    def __init__(self):
+        self.name = "name"
+        self.common_name = "common_name"
+        self.len = 0
+        self.nfrags = 0
+
+    def __str__(self):
+        rep = '{0} len: {2} \n'\
+              .format(self.name, 
+                      self.len )
+
+        return rep
+
+    pass
+    # End Gene
+
+
+def main(argv = None):
+    if argv is None:
+        argv = sys.argv
+
+    mindepth = None
+    try:
+        opts, args = getopt.getopt(argv, "n:hd", ["help", "output="])
+    except getopt.GetoptError, msg:          
+        usage(msg)                         
+        return 2
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):      
+            usage()                     
+            sys.exit()                  
+        elif opt == '-n':
+            mindepth = int(arg)                  
+        elif opt == '-d':                
+            global _debug               
+            _debug = 1                  
+        else:
+            usage()                         
+            return 2
+
+
+    n_mapped_frags = 0
+    genes = dict()
+    reader = csv.DictReader(sys.stdin, delimiter='\t')
+    
+    for row in reader:
+        gname = row["refseq"]
+        if not (gname in genes):
+            gene = Gene()
+            gene.name = row["refseq"]
+            gene.len = row["genelen"]
+            genes[gname] = gene
+        else:
+            gene = genes[gname]
+        gene.nfrags = gene.nfrags + 1
+        n_mapped_frags = n_mapped_frags + 1
+
+    # if the user supplied a value for mindepth (via command-line option),
+    # filter out any gene that does not have at least that many
+    # reads.
+
+    if mindepth is not None:
+        for key in genes.keys():
+            gene = genes[key]
+            avgdepth = gene.nfrags/gene.len
+            if avgdepth < mindepth:
+                n_mapped_frags = n_mapped_frags - gene.nfrags
+                del genes[key]
+                
+    #
+    # Calculate FPKM for each remaining gene and print the results.
+    #
+    hdr = "{0:s}\t{1:s}"
+    rec = "{0}\t{1:f}\t{2:f}"
+
+    print hdr.format("tracking_id", "FPKM")
+    
+    m = float(n_mapped_frags)/(float(1000000))
+    for name in genes.keys():
+        gene = genes[name]
+        k = (float(gene.len)/float(1000))
+        # print name, gene.len, k, m, n_mapped_frags
+        fpk = (float(gene.nfrags) / k)
+        fpkm = fpk / m
+        avgdepth = float(gene.nfrags)/float(gene.len)
+
+        #print name, gene.nfrags, fpkm, k, m
+        print rec.format(name, fpkm, avgdepth)
+    
+
+def usage(msg = None):
+    if msg is not None:
+        print msg
+    print __name__, "[-n <depth>] < <input file> "
+
+   
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
---
+++ scripts/cuffscatter.py
@@ -0,0 +1,213 @@
+# cuffscatter.py
+# 
+#
+# Compare two datasets by making a scatterplot of both.
+# This is an incredibly unsophisticated script that just creates a
+# scatter plot from two sets of data.  No complicated processing or
+# filtering is happening here.
+#
+# The input files must be text files, each having a number of columns
+# seperated by a single tab, and the first row must be a header with
+# the names of the columns.  Two of the columns must be labelled
+# 'tracking_id' and 'FPKM'.  Other columns will be ignored.
+#
+# The following would be a valid format for the input files,
+#   tracking _id  col2    col3    FPKM
+#   gene1         ajunk   bjunk   10.292
+#   gene2         junkk   jjunk   0.2e-12
+#
+# The columns must be labelled, seperated by tabs, and may be in any
+# order, and may include junk columns.  
+#
+# usage: python ../../scripts/cuffscatter.py input_A input_B
+#
+# The input_A and input_B may be the output of cufflinks
+# ('genes.fpk.tracking') or they may be the output of more stringent
+# processing by 'rpos_dist.py' followed by 'calculate_fpkm.pk'
+#
+# Options:
+#
+# -x "label"	Use 'label' for the X axis label; default is no label
+# -y "label"	Use 'label' for the Y axis label; default is no label
+# -t "title"	Use 'title' for the title of the plot; default is none,
+#		or if -x and -y are supplied, then a title will be constructed
+#		from the X- and Y- labels.
+# -n <num>	Graph only the top <num> most abundant genes.	
+#
+# -m <marker>	Mark each point with <marker>; default is a single pixel mark.
+# 		Use 'o' for a larger round mark that is easier to see.
+#		<marker> should be one of the matplotlib line markers documented at
+# 		http://matplotlib.sourceforge.net/api/artist_api.html#matplotlib.lines.Line2D.set_marker
+#
+
+
+import csv
+import sys
+import getopt
+import math
+import string
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib.ticker as ticker
+
+import numpy as np
+from scipy import stats
+import pylab
+from array import array
+
+
+
+# main() takes an optional 'argv' argument, which allows us to call it
+# from the interactive Python promp.
+
+def main(argv = None):
+    if argv is None:
+        argv = sys.argv
+
+    xlabel = None
+    ylabel = None
+    title = None
+    nsamples = None
+    marker=','
+    
+    try:
+        opts, args = getopt.getopt(argv, "hx:y:t:n:m:d", ["help", "output="])
+    except getopt.GetoptError, msg:          
+        usage(msg)                         
+        return 2
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):      
+            usage()                     
+            sys.exit()                  
+        elif opt == '-d':                
+            global _debug               
+            _debug = 1                  
+        elif opt == '-x':                
+            xlabel = arg                  
+        elif opt == '-y':                
+            ylabel = arg                  
+        elif opt == '-t':                
+            title = arg                  
+        elif opt == '-n':                
+            nsamples = int(arg)
+        elif opt == '-m':
+            if arg.isdigit():
+                marker = int(arg)
+            else:
+                marker = arg
+        else:
+            usage()                         
+            return 2
+
+
+    cuffout1 = args[0]
+    cuffout2 = args[1]
+
+
+    # read in the list of x,y points for the plot from stdin
+    #
+    genes1 = dict()
+    in_handle = None
+    try:
+        in_handle = open(cuffout1)
+        
+        g_reader = csv.DictReader(in_handle, delimiter='\t')
+        for g_row in g_reader:
+            genes1[g_row["tracking_id"]] = g_row["FPKM"]
+
+    finally:
+        if in_handle is not None:
+            in_handle.close()
+
+
+    # read in the list of x,y points for the plot from stdin
+    #
+    genes2 = dict()
+    in_handle = None
+    try:
+        in_handle = open(cuffout2)
+        
+        g_reader = csv.DictReader(in_handle, delimiter='\t')
+        for g_row in g_reader:
+            genes2[g_row["tracking_id"]] = g_row["FPKM"]
+
+    finally:
+        if in_handle is not None:
+            in_handle.close()
+
+    keys = list()
+    prospective_keys = sorted(genes1, key=lambda key: float(genes1[key]), reverse=True)
+    for k in prospective_keys:
+        if k in genes2:
+            keys.append(k)
+
+    if nsamples is not None:
+        nsamples = min(nsamples, len(keys))
+    else:
+        nsamples = len(keys)
+        
+    # print keys[:nsamples]
+    
+    x = list()
+    y = list()
+    for k in keys[:nsamples]:
+        x.append(float(genes1[k]))
+        y.append(float(genes2[k]))
+        
+    global fig
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111)
+
+
+    # You might think that the way to get a log-log scatter plot
+    # would be to use scatter() and set the axis scales to "log"
+    # That doesn't work.  It ends up graphing only a portion of the data.
+    # 	ax.set_yscale('log')
+    #   ax.set_xscale('log')
+    #   ax.set_xlim(1e-1, 10e5)
+    #   ax.set_ylim(1e-1, 10e5)
+    #   plt.scatter(x, y)
+    #
+    # Calling plt.loglog() works though.
+    
+    plt.loglog(x,y,marker=marker,linestyle='none')
+
+    # calculate regression statistics that we can display on the plot.
+    #
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+        
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+
+
+    # Add a legend to display statistical measures.
+    # Note that there is no guarantee that this will not overlap a datapoint.
+    #
+    ax.text(0.05, 0.9, 'R$\r{^{2}}$=%0.4f\nN=%d'%(r_value**2, len(x)),
+            transform=ax.transAxes, va='top')
+    
+    # Provide a default label if the user did not supply one.
+    if title is None and xlabel is not None and ylabel is not None:
+        title = "Comparison of %s and %s" % (xlabel, ylabel)
+        
+    if title is not None:
+        ax.set_title(title)
+
+    #fig.savefig("fig.%s.png" % (ylabel), format="png")
+    
+    plt.show()
+
+    return 0
+
+def usage(msg = None):
+    if msg is not None:
+        print msg
+    print __name__, "[-x <xlabel>] [-y <ylabel>] [-t <title>] [-m <marker>] [-n <#points>] <input_1.fpkm> <input_2.fpkm> "
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
