ca50cbb34028238ec5e22c4202d2ed650ed7f6d0
cswarth <cswarth@gmail.com>
2013-03-23 22:25:03 -0700

    Unify arguments to scatter and maplot; use text instead of testxy to gain more control over labl positions.
    - scatter and maplot now take the same set of arguments for specifying report depth and for pulling out specific genes.
    - test labels are not positioned closer to the point they label.

--- scripts/R/morrislib.R
+++ scripts/R/morrislib.R
@@ -22,7 +22,7 @@ suppressMessages( library(plotrix) )
 morris.fetchData <- function(datasets, group=NULL) {
     # SQL query for retrieving the knowngene list.  The proper genome is selected from
     # the first dataset.
-    kquery <- paste0("select distinct name,name2 from knowngenes2 k ",
+    kquery <- paste0("select distinct name,name2 as gene from knowngenes2 k ",
                      "join (select genome from experiments_tbl e join datasets_tbl d ",
                      "on d.expr_id=e.id where d.name like '", datasets[1],
                      "%') g on g.genome=k.genome");
@@ -62,12 +62,12 @@ morris.fetchData <- function(datasets, group=NULL) {
         ds <- lapply(datasets, f)
 
         ## merge all the experimental results into a single dataframe based on
-        ## the refseq gene name found in the "name" column.
+        ## the refseq gene name found in the "refseq" column.
         ##
         ## For details, see
         ## http://stackoverflow.com/questions/8091303/merge-multiple-data-frames-in-a-list-simultaneously
         ## 
-        df = Reduce(function(...) merge(...,all=T), ds)
+        df = Reduce(function(...) merge(...,all=T, by="name", suffixes=c("", ".2")), ds)
 
         # assert that there are no duplicate gene names in the list.
         stopifnot(nrow(df[duplicated(df[,1]),]) == 0)
@@ -143,7 +143,8 @@ morris.fetchdesc <- function(datasets, group=NULL) {
 ## are at a remote location and you have set up a tunnel via ssh to
 ## the database at UW.
 
-morris.maplot <- function(datasets, mincount=25, group=NULL, normalization="quantile", genes=NULL) {
+morris.maplot <- function(datasets, mincount=25, group=NULL, normalization="quantile",
+                          genes=NULL, report=5) {
     df = morris.genecounts(datasets, mincount=mincount, group=group)
 
     ## normalize to total reads in each experiment.
@@ -164,6 +165,7 @@ morris.maplot <- function(datasets, mincount=25, group=NULL, normalization="quan
                           lg2A=log2(x[dexpression,1]),
                           lg2B=log2(x[dexpression,2]),
                           diff=(x[,2]-x[,1])[dexpression])
+    rownames(results) <- rownames(df)[dexpression]
     results <- results[order(-results$diff),]
 
     ## nf <- layout(matrix(c(1,2), 1, 2, byrow <- TRUE), widths=c(2,1), heights=c(1,1))
@@ -174,51 +176,91 @@ morris.maplot <- function(datasets, mincount=25, group=NULL, normalization="quan
     title(paste0(descriptions[,1]," (",rownames(descriptions),")",
                  collapse="\n vs. "))
     textxy(rowMeans(log2(x))[dexpression],(log2(x[, 1])-log2(x[, 2]))[dexpression],
-           df[dexpression,1], dcol="red")
+           df[dexpression,'gene'], dcol="red")
   
   
   return (results)
 }
 
 
-
+##
+## Returns a table of the most over-/under-expressed genes.
+##
+## datasets - the names of datasets in the MySQL database to use, e.g. c("030713_A", "103112_B")
+##
+## mincount - filter out genes if any dataset has fewer then mincount raw reads before scaling.
+##
+## normlization - the type of nomalization to apply.
+##
+## report - this many over/under expressed genes.
+##
+## genes - extra genes to highlight, in addition to the top over/under expressed, e.g. c("Pbsn", "Sbp")
+##
 morris.scatter <- function(datasets, mincount=25, group=NULL,
-                           normalization= c( "scaled", "quantile"), report=5) {
+                           normalization=c("rpm", "scaled", "quantile"),
+                           genes=NULL, report=5, logscale=FALSE) {
     df = morris.genecounts(datasets, mincount=mincount, group=group)
 
     ## normalize to total reads in each experiment.
     normalization <- match.arg(normalization)
     x<- switch(normalization,
+               rpm = df[2:ncol(df)]/(colSums(df[2:ncol(df)]/(10^6))),
                scaled = df[2:ncol(df)]/colSums(df[2:ncol(df)]),
                quantile = data.frame(normalize.quantiles(data.matrix(df[2:ncol(df)]))))
 
     ## identify the entries with the highest and lowest fold changes
     dexpression <- order(x[,2]-x[,1])[1:report]
     dexpression <- append(dexpression, order(x[,1]-x[,2])[1:report])
+    dexpression <- append(dexpression, match(genes, df[,1]))
+    
   
     results <- data.frame(gene=df[dexpression,1],
                           rawA=df[dexpression,2],
                           rawB=df[dexpression,3],
                           normA=x[dexpression,1],
                           normB=x[dexpression,2],
-                          lg2A=log2(x[dexpression,1]),
-                          lg2B=log2(x[dexpression,2]),
                           diff=(x[,2]-x[,1])[dexpression])
+    rownames(results) <- rownames(df)[dexpression]
     results <- results[order(-results$diff),]
 
     ## nf <- layout(matrix(c(1,2), 1, 2, byrow <- TRUE), widths=c(2,1), heights=c(1,1))
     descriptions = morris.fetchdesc(datasets, group=group)
-        
-    plot( x[,1], x[,2], log="xy", cex=.8,
-         xlab=descriptions[datasets[1],1],
-         ylab=descriptions[datasets[2],1])
-    abline(lm(log10(x[,2])~0+log10(x[,1])), col="red");
+    if (logscale==TRUE) {
+        plot( x[,1], x[,2], cex=.5, log="xy",
+             panel.first = grid(4,4),
+             xlab=paste0(descriptions[datasets[1],1], " (log)"),
+             ylab=paste0(descriptions[datasets[2],1], "(log)"))
+        abline(lm(log10(x[,2])~0+log10(x[,1])), col="red")
+
+    } else {
+        plot( x[,1], x[,2], cex=.5,
+             xlab=descriptions[datasets[1],1],
+             ylab=descriptions[datasets[2],1])
+        abline(lm(x[,2]~0+x[,1]), col="red")
+    }
     title(paste0(descriptions[,1]," (",rownames(descriptions),")",
                  collapse="\n vs. "))
-    textxy(x[dexpression,1],x[dexpression,2], df[dexpression,1], dcol="red")
-    
-        
+    text(x[dexpression,],  df[dexpression,'gene'], adj=c(-0, -0.3), cex=.7, col="red")
+
     return (results)
+}
+
+
+## A function to use identify to select points, and overplot the
+## points with another symbol as they are selected
+identifyPch <- function(x, y = NULL, n = length(x), pch = 19, ...)
+{
+    xy <- xy.coords(x, y); x <- xy$x; y <- xy$y
+    sel <- rep(FALSE, length(x)); res <- integer(0)
+    while(sum(sel) < n) {
+        ans <- identify(x[!sel], y[!sel], n = 1, plot = FALSE, ...)
+        if(!length(ans)) break
+        ans <- which(!sel)[ans]
+        points(x[ans], y[ans], pch = pch)
+        sel[ans] <- TRUE
+        res <- c(res, ans)
+    }
+    return(res)
 }
 
 ## retrieve a list of dataset names that have per-gene expression values stored in the database.
