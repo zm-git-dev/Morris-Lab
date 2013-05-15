60001a1d7885cecb05b5deddfcda7931ead4ac17
cswarth <cswarth@gmail.com>
2013-05-14 20:30:41 -0700

    Add code to scale transcripts to amino acids (instead of nucleotides)

--- scripts/R/profiling/multijpeg.R
+++ scripts/R/profiling/multijpeg.R
@@ -10,19 +10,34 @@ gene="NM_007409"   # RPL22
 gene='NM_016978'	# 'Oat'
 gene="NM_007409"   # ADH1
 
-genes = c('NM_001005419', 'NM_013541', 'NM_011434', 'NM_144903', 'NM_016978', "NM_007409")
+profiles = list(
+NM_007409=list(list(15,115), list(90,190), list(176,276), list(220,320), list(260,360)), 
+NM_011434=list(list(15,115)),
+NM_013541=list(list(1,80)),
+NM_011044=list(list(30,130), list(100,200), list(160,260), list(520,620))
+)
 
-for (gene in genes) {
+
+for (gene in names(profiles)) {
     df = morris.getalignments("113010_A", gene)
-        
     kg <- morris.getknowngenes(attr(df, "genome"), gene=gene, group=NULL)
     rownames(kg) <- kg$name
+
     gobj = transcript(kg[gene,])
 
-    jpeg(filename = paste0("Rplot_", gobj$name2(), ".jpeg"), width=1200, height=909,type="quartz")
-    
-    par(mar=c(1,2,1,2))
-    profile(df, gobj, bias="middle", minlen=28)
-    dev.off()
+    for (x in profiles[[gene]]) {
+        xlim=as.numeric(x)
+        jpeg(filename = paste0("Rplot_", gobj$name2(), "_", xlim[1], "-", xlim[2], ".jpeg"),
+             width=1200, height=909,type="quartz")
+
+        ## set the margin to give a more pleasing layout.
+        ##     c(bottom, left, top, right) gives the number of
+        ##     lines of margin to be specified on the four sides of the plot
+        par(mar=c(1,2,1,2))
+        print(xlim)
+        profile(df, gobj, bias="middle", minlen=28, xlim=xlim, units="aa")
+        dev.off()
+    }
+
 }
 
--- scripts/R/profiling/profiling.R
+++ scripts/R/profiling/profiling.R
@@ -56,18 +56,21 @@ morris.getalignments <- function(dataset, gene, group=NULL) {
 ## This awesome flavor of OOP in R using closures is taken from a
 ## stackoverflow posting I stumbled across:
 ## http://stackoverflow.com/a/15245568/1135316
+
+##
+## I've since realize that this is just the S3 style of classes in R.
 ## Add a few lines and it is now an S3 style class.
 transcript = function(gdata) {
     starts <- as.numeric(strsplit(gdata$exonStarts,",")[[1]])
     ends <- as.numeric(strsplit(gdata$exonEnds,",")[[1]])
     elen <- ends - starts
-    print(elen)
     transcriptLength <- sum(elen)
-    print(transcriptLength)
+    ##print(transcriptLength)
 
     cdsStart = function() rpos(if (gdata$strand == '+') { gdata$cdsStart } else { gdata$cdsEnd })
     cdsEnd = function() rpos(if (gdata$strand == '+') { gdata$cdsEnd-1 } else { gdata$cdsStart+1 })
     cdsLength = function() cdsEnd()-cdsStart()+1
+    peptideLength = function() cdsLength()/3
     txStart = function()  rpos(if (gdata$strand == '+') { gdata$txStart } else { gdata$txEnd })
     txEnd = function() rpos(if (gdata$strand == '+') { gdata$txEnd-1 } else { gdata$txStart+1 })
     length = function() txEnd()-txStart()+1
@@ -104,14 +107,15 @@ transcript = function(gdata) {
     }
     
 
-    cat(paste(name(), "\n"))
-    cat(paste("\tcdsStart", cdsStart(), "\n"))
-    cat(paste("\tcdsEnd", cdsEnd(), "\n"))
-    cat(paste("\tcds span", cdsLength(), "\n"))
+    ## cat(paste(name(), "\n"))
+    ## cat(paste("\tcdsStart", cdsStart(), "\n"))
+    ## cat(paste("\tcdsEnd", cdsEnd(), "\n"))
+    ## cat(paste("\tcds span", cdsLength(), "\n"))
 
-    cat(paste("\ttxStart", txStart(), "\n"))
-    cat(paste("\ttxEnd", txEnd(), "\n"))
-    cat(paste("\ttx span", length(), "\n"))
+    ## cat(paste("\ttxStart", txStart(), "\n"))
+    ## cat(paste("\ttxEnd", txEnd(), "\n"))
+    ## cat(paste("\ttx span", length(), "\n"))
+    stopifnot(txStart() == 1)
     
     exported = list(
       length=length,
@@ -123,7 +127,8 @@ transcript = function(gdata) {
       isCoding=isCoding,
       name=name,
       name2=name2,
-      rpos=rpos
+      rpos=rpos,
+      peptideLength=peptideLength
       )
     class(exported) <- "transcript"
     invisible(exported)
@@ -133,22 +138,31 @@ print.transcript <- function(object) {
     cat("transcript ", object$name2(), ", ", object$length(), "\n")
 }
 
-plot.transcript <- function(self, xlim=NULL) {
-    if (is.null(xlim))
-      xlim <- c(1, self$length())
-
+plot.transcript <- function(self, xlim=NULL, units="nucleotide") {
+    if (units == "aa") {
+        if (is.null(xlim)) 
+          xlim <- c(self$cdsStart(), self$cdsEnd())
+    } else {
+        if (is.null(xlim))
+          xlim <- c(1, self$length())
+    }
+    
     cdsHeight = 10   # height of coding region, expressed as percentage of plot height
     if (self$isCoding()) {
         ## Label the endpoints of the coding region and place the gene
         ## name in the middle of the coding region.
-        cdslim = c(max(xlim[1], self$cdsStart()), min(xlim[2], self$cdsEnd()))
+        if (units == "aa") {
+            cdslim = c(max(xlim[1], 1), min(xlim[2], self$peptideLength()))
+        } else {
+            cdslim = c(max(xlim[1], self$cdsStart()), min(xlim[2], self$cdsEnd()))
+        }
         text((cdslim[1] + cdslim[2])/2, 100-2*cdsHeight-5,
              adj=c(.5,1.0), self$name2())
         text(cdslim[1], 100-2*cdsHeight-5, adj=c(.5,1.0), as.character(cdslim[1]))
         text(cdslim[2], 100-2*cdsHeight-5, adj=c(.5,1.0), as.character(cdslim[2]))
         clip(xlim[1], xlim[2], 0, 100)
         segments(xlim[1], 100-cdsHeight,  xlim[2],  100-cdsHeight, lwd=5, lend="butt")
-        rect(self$cdsStart(), 100-2*cdsHeight, self$cdsEnd(), 100, col='blue')
+        rect(cdslim[1], 100-2*cdsHeight, cdslim[2], 100, col='blue')
     } else {
         ## There is no coding region - this is a non-coding gene.
         ## Label the endpoints of the transcript and label the gene in the middle.
@@ -161,7 +175,7 @@ plot.transcript <- function(self, xlim=NULL) {
     }
 }
 
-profile <- function(df, transcript, xlim=NULL, bias="start", minlen=NULL) {
+profile <- function(df, transcript, xlim=NULL, units="nucleotide", bias="start", minlen=NULL) {
     usr = par()$usr
     plt = par()$plt
 
@@ -180,12 +194,21 @@ profile <- function(df, transcript, xlim=NULL, bias="start", minlen=NULL) {
 
     ## if the user is zooming in on a portion of the transcript, set the
     ## limits of the horizontal axis accordingly.   Otherwise the limits
-    ## are determined by the lengt of the trnscript.
-    if (is.null(xlim))
-      xlim <- c(1, transcript$length())
+    ## are determined by the length of the transcript.
+    if (units == "aa") {
+        if (is.null(xlim)) {
+            xlim = c(transcript$cdsStart(), transcript$cdsEnd())
+        } 
+        ## convert nucleotide positions to codon position
+        x = (x - transcript$cdsStart())/3
+    } else {
+        if (is.null(xlim))
+          xlim <- c(1, transcript$length())
+    }
 
+    
     ## discard any alignments that fall outside the horizontal limits.
-    ## (hist() will complain if you don't do it)
+    ## ( why?  won't the plot just truncate any data outside the range? )
     is.between <- function(x,lim) {
         (x > lim[1]) & (x < lim[2])
     }
@@ -199,10 +222,14 @@ profile <- function(df, transcript, xlim=NULL, bias="start", minlen=NULL) {
 
     ## Calculate the histogram bins and maximum count across all bins.
     ##
-    binsize <- 1   ## for small transcripts with few reads, use a bin size of one.
-    if ((xlim[2]-xlim[1]) > 100) 
-        binsize <- 3  ## otherwise use a bin size of three
-    histdata <- hist(x, breaks=seq(1,transcript$length(), binsize), plot=F)
+    if (units == "aa") {
+        ## for small transcripts with few reads, use a bin size of one.
+        breaks=seq(1, transcript$peptideLength(), 1)
+    } else {
+        ## otherwise use a bin size of three
+        breaks=seq(1,transcript$length(), 3)
+    }
+    histdata <- hist(x, breaks=breaks, plot=F)
     maxy <- max(histdata$count)
 
     plot(histdata$mids[histdata$counts != 0],histdata$counts[histdata$counts != 0], type='h',
@@ -222,7 +249,7 @@ profile <- function(df, transcript, xlim=NULL, bias="start", minlen=NULL) {
     ## Draw the transcript in the lower 1/3 of the plot area
     par(plt=c(plt[1], plt[2], plt[3], plt[3]+(plt[4]-plt[3])/3))
     par(usr=c(tmp[1], tmp[2], 0, 100))
-    plot(transcript, xlim=xlim)
+    plot(transcript, xlim=xlim, units=units)
     par(usr=usr)
     par(plt=plt)
 
@@ -232,14 +259,14 @@ profile <- function(df, transcript, xlim=NULL, bias="start", minlen=NULL) {
 .pardefault <- par(no.readonly = T)
 
 #gene="NR_029560"   # Mir150
-gene='NM_001005419'	# 'Ado'  single exon reverse
-gene='NM_013541'	# 'Gstp1'
-gene="NM_007409"   # ADH1
 #gene='NM_TEST'		# 'test'
 gene='NM_016978'	# Oat '-'
 gene='NM_144903'	# Aldob '-'
 gene='NM_011434'	# 'Sod1'
 gene='NM_011044'	# 'Pck1'
+gene='NM_013541'	# 'Gstp1'
+gene='NM_001005419'	# 'Ado'  single exon reverse
+gene="NM_007409"   # ADH1
 
 plot.new()
 
@@ -253,15 +280,13 @@ kg <- morris.getknowngenes(attr(df, "genome"), gene=gene, group=NULL)
 ##                 cdsStart=c(5,6), cdsEnd=c(8,9), exonCount=1, exonStarts="1,", exonEnds="20,",
 ##                 name2=c("test","test2"),stringsAsFactors = F)
 rownames(kg) <- kg$name
-print(kg[gene,])
+##print(kg[gene,])
 gobj = transcript(kg[gene,])
-x = sapply(X=df$position, FUN=gobj$rpos)
-print(head(x))
 
 #df <- data.frame(position=c(49562310,49562310), length=22)
-attr(df,"genome") <- "mm9"
+#attr(df,"genome") <- "mm9"
 
-profile(df, gobj, bias="middle")
-##    profile(df, gobj, xlim=c(80,200))
+profile(df, gobj, bias="middle", minlen=28, units="aa", xlim=c(15, 115))
+
 
 par(.pardefault)
