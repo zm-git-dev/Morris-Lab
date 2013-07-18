667dca2f64595cf04b423d39e5bc7ae0f88f3e8f
cswarth <cswarth@gmail.com>
2013-07-18 14:59:03 -0700

    - Reconfigure to be function definitions only (more usrful to include in shiny apps) - Add generic summary method to profile class.

--- scripts/R/profiling/profiling.R
+++ scripts/R/profiling/profiling.R
@@ -52,6 +52,7 @@ transcript = function(gdata) {
         pos = pos + starts[i]
         return(pos)
     }
+    
     rpos = function(pos) {
         ## Given a position on a chromosome, calculate the position
         ## relative to the start of transcript.
@@ -238,6 +239,15 @@ profile <- function(df, gene.data) {
     invisible(exported)
 }
 
+# Summary method
+# Should return a summary object (and not print it!)
+# Need a unique class for it ("fsummary")
+summary.profile <- function(self,...) {
+    res <- summary(self$alignments())
+    res
+}
+
+
 ## specialization of generic print function for instances of 'profile' class.
 print.profile <- function(this) {
     print(this$transcript())
@@ -360,33 +370,4 @@ plot.profile <- function(self, xlim=NULL, units="nucleotide", bias="middle", min
 }
 
 
-#gene="NR_029560"   # Mir150
-#gene='NM_TEST'		# 'test'
-gene='NM_144903'	# Aldob '-'
-gene='NM_011434'	# 'Sod1'
-gene='NM_013541'	# 'Gstp1'
-gene='NM_001005419'	# 'Ado'  single exon '-'
-gene="NM_007409"   # ADH1
-gene="NR_029600"   # Mir122a   single exon '+'
-gene="NM_133862"    ## Fgg
-gene='NM_016978'	# Oat '-'
-gene='NM_009790'    ## CALM1 - Calmodulin - no B-sheets
-gene ="NM_009654" ## Alb
-gene='NM_011044'	# 'Pck1'
-gene='NM_016978'    # Oat '-'
-gene='NM_001005419'    # 'Ado'  single exon '-'
-gene="NM_007409"   # ADH1
-gene="NM_010321"  
-
-plot.new()
-
-df = morris.getalignments("113010_A", gene)
-
-kg <- morris.getknowngenes(attr(df, "genome"), gene=gene, group=NULL)
-rownames(kg) <- kg$name
-
-p = profile(df, kg[gene,])
-
-print(p)
-print(plot(p, minlen=28))
 
