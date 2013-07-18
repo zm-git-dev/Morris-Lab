ec92f16a0a18a37e1a44939505bb09979c4cc35a
cswarth <cswarth@gmail.com>
2013-07-18 14:52:23 -0700

    Add scatter plot routine based  on prostate scatter plot but without all the epithelial and fibroblast filtering.

--- scripts/R/CLL Analysis/scatter.r
+++ scripts/R/CLL Analysis/scatter.r
@@ -1,52 +1,64 @@
+source("../morrislib.R")
+options("max.print"=30)
+
 library(Biobase)
 require(genefilter)
 
-epithelial <- c("9530053A07Rik", "Tgm4", "Pbsn", "Rnase1", "Sbp")
-fibroblast <- c("Vim", "Itgb1", "Itga1", "Col1a1", "Col1a2")
+norm = "rpm"
+mincount = 30
 
 ## treated <- morris.datasets(organism="Human", tissue="CLL", genotype="Ribo+/Col+")
-treated <- c("061113_B")
 control <- c("061113_A")
+treated <- c("061113_D")
 datasets <- c(treated, control)
 
 
-ds = morris.genecounts(datasets, group=NULL)
-genome = attr(ds, "genome")
+df <- morris.genecounts(datasets, group=NULL)
+genome <- attr(df, "genome")
 
-df = ds
 ## Get rid of any rows containing NA.
-df = df[complete.cases(df),]
+#df <- df[complete.cases(df),]
 
-## Exclude genes that are highly expressed in epithelial genes.
-##cn <- morris.commonnames(rownames(df), genome, NULL) 
-##df = df[!(cn %in% epithelial),]
-
-## Exclude genes that have too few reads.
-## row.sub = apply(df[,grep("103112",names(df),value=TRUE),drop = FALSE], 1, function(row) (any(row >= 30)))
-## row.sub = row.sub & apply(df[,grep("030713",names(df),value=TRUE),drop = FALSE], 1, function(row) any(row >= 50))
-## row.sub = row.sub & apply(df[,grep("032513",names(df),value=TRUE),drop = FALSE], 1, function(row) any(row >= 100))
-## df = df[row.sub,]
+## Identify entries that do not have proper read depth, but don't
+## eliminate them yet.  Some normalization methods rely upon
+## having the full dataset available, even if only a portion of
+## the dataset will eventually be reported.
+##
+row.sub = NA
+if (!is.na(mincount)) {
+    ## row.sub is the list of rows to KEEP!
+    row.sub = apply(df, 1, function(row) (all(!is.na(row)) && all(row >= mincount)))
+}
 
 ## Normalize to mapped reads per million mapped reads
-##  http://stackoverflow.com/questions/13830979/#13831155
-df = sweep(df,2,(colSums(df)/(10^6)), '/')
+df <- morris.normalize(df, norm)
 
-## ## scale all RPM count to the RPM count of Col1a2
-## ## 	First lookup the values for Col1a2
-## cn <- morris.commonnames(rownames(df), genome, NULL) 
-## reference = df[match("Col1a2", cn),]
-## ##	Now divide each column in each row by to corresponding column of the reference.
-## ##	http://stackoverflow.com/questions/13830979/#13831155
-## df <- sweep(df,2,as.numeric(reference), '/')
+## Now remove the entries identified earlier that do not have
+## proper read depth.
+##
+if (!is.na(row.sub))
+    df <- df[row.sub,, drop=FALSE]
 
 ## ## Save the refseq names of fibroblast genes that we will want to highlight
 ## hilite=names(cn[match(fibroblast,cn)])
+kg = morris.getknowngenes(genome)
+sno = grep("SNOR", kg$name2)
+notsno = grep("SNOR", kg$name2, invert=TRUE)
 
-e=(df)
-m1=e[,control]
-m2=e[,treated]
-plot(m1, m2, xlab=paste0(control, collapse="-"), ylab=paste0(treated, collapse="-"),log="xy")
-tstr = paste("control (", length(control), ") vs treated (", length(treated), ")")
-title(tstr, sub="min=50, rpm",cex.main=.7,
-      cex.sub=.6, col.sub="grey73")
+m = df[match(kg$name[notsno], rownames(df)),]
+m1=m[, control]
+m2=m[, treated]
+plot(m1, m2, xlab=paste0(control, collapse="-"),
+     ylab=paste0(treated, collapse="-"),log="xy", pch=16, cex=0.7)
 
+m = df[match(kg$name[sno], rownames(df)),]
+m1=m[, control]
+m2=m[, treated]
+points(m1, m2,col='blue', pch=17, cex=1)
+
+descriptions = morris.fetchdesc(datasets, group=group)
+tstr = paste0(descriptions[,1], collapse="\n vs. ")
+
+
+# tstr = paste("control (", length(control), ") vs treated (", length(treated), ")")
+title(tstr, sub=paste0("SNORNA", norm, collapse=", "),cex.main=.9, cex.sub=.6, col.sub="grey73")
