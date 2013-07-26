85b46cdbd5c39e9422ced1c0e2b5daf5ed532e96
cswarth <cswarth@gmail.com>
2013-07-25 18:27:56 -0700

    know

--- scripts/R/Prostate analysis/scatter.r
+++ scripts/R/Prostate analysis/scatter.r
@@ -1,6 +1,9 @@
 library(Biobase)
 require(genefilter)
 
+tumorSpecific <- c( "Tubb5", "Hist2h2bb", "Nov", "Chgb", "Tmsb4x",
+                   "Hist1h1a", "Dpysl2", "Mki67", "Chga", "Uchl1", "Rrm1",
+                   "Lmnb1", "Cpe", "Eef1a2", "Thbs1")
 epithelial <- c("9530053A07Rik", "Tgm4", "Pbsn", "Rnase1", "Sbp")
 fibroblast <- c("Vim", "Itgb1", "Itga1", "Col1a1", "Col1a2")
 
@@ -12,10 +15,7 @@ df = morris.genecounts(datasets, group=NULL)
 genome = attr(df, "genome")
 
 ## Get rid of any rows that are missing data...
-## Hmmmmm.   Maybe should keep those and just set them to zero?
-##
 df[is.na(df)] <- 0
-##df = df[complete.cases(df),]
 
 ## Identify genes that have too few reads.
 row.sub = apply(df[,grep("103112",names(df),value=TRUE),drop = FALSE], 1, function(row) (any(row >= 30)))
@@ -31,7 +31,7 @@ stopifnot(attr(df,"genome") == genome)
 attr(df,"genome") = genome
 df.raw = df
 attr(df.raw,"genome") = genome
-df.norm = morris.normalize(df.raw, normalization="rpm")
+df.norm = morris.normalize(df.raw, normalization="TC")
 
 ## scale all RPM count to the RPM count of Col1a2
 ## 	First lookup the values for Col1a2
@@ -50,25 +50,28 @@ cn <- cn[row.sub,, drop=FALSE]
 ## Save the refseq names of fibroblast genes that we will want to highlight
 rows.epithelial <- rownames(df.norm)[cn$common %in% epithelial]
 rows.fibroblast <- rownames(df.norm)[cn$common %in% fibroblast]
-rows.neither <- rownames(df.norm)[!cn$common %in% c(fibroblast, epithelial)]
+rows.tumorSpecific <- rownames(df.norm)[cn$common %in% tumorSpecific]
+rows.neither <- rownames(df.norm)[!cn$common %in% c(fibroblast, epithelial, tumorSpecific)]
     
 m1=log2(rowMeans(df.norm[,control]))
 m2=log2(rowMeans(df.norm[,treated]))
 
 xlim=c(min(m1), max(m1))
 ylim=c(min(m2), max(m2))
-plot(m1[rows.neither], m2[rows.neither], xlab="Control", ylab="Tramp+",
+plot(m1[rows.neither], m2[rows.neither],
+     xlab="Control", ylab="Tramp+",
      cex=.7, xlim=xlim, ylim=ylim)
 tstr = paste("Prostate Control (", length(control), ") vs TRAMP+ (", length(treated), ")")
 ststr = "prostate Analysis/scatter.R, rpm,  norm(Col1a2), mean vs. mean"
 title(tstr, sub=ststr, cex.main=1, cex.sub=1, col.sub="grey73")
     
-points(m1[rows.epithelial], m2[rows.epithelial], pch=18, cex=1, col="red")
-points(m1[rows.fibroblast], m2[rows.fibroblast], pch=17, cex=1, col="blue")
-
+points(m1[rows.epithelial], m2[rows.epithelial], pch=18, cex=1.2, col="red")		## diamond
+points(m1[rows.fibroblast], m2[rows.fibroblast], pch=17, cex=1.2, col="blue")		## triangle
+points(m1[rows.tumorSpecific], m2[rows.tumorSpecific], pch='+', cex=1.2, col="green")	## square
 
 textxy(m1[rows.epithelial], m2[rows.epithelial], cn[rows.epithelial,"common"], dcol="red")
 textxy(m1[rows.fibroblast], m2[rows.fibroblast], cn[rows.fibroblast,"common"], dcol="blue")
+textxy(m1[rows.tumorSpecific], m2[rows.tumorSpecific], cn[rows.tumorSpecific,"common"], dcol="green")
 Sys.sleep(0) 
 
 repeat {
